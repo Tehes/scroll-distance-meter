3ef881778eefcfd8e3f63827e0d1f59f918e33f9
Tino Sabetta <tino.sabetta@gmail.com>
2024-11-17 19:01:23 +0100

    Ífix: ensure scroll meter dynamically adjusts width to content

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ function createScrollMeterElement() {
     scrollMeterElement.style.boxShadow = "rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px";
     scrollMeterElement.textContent = "0.00 m";
     scrollMeterElement.style.zIndex = "2147483647";
+    scrollMeterElement.style.whiteSpace = "nowrap";
+    scrollMeterElement.style.width = "fit-content";
+
+    scrollMeterElement.textContent = "0.00 m";
 
     document.body.appendChild(scrollMeterElement);
     return scrollMeterElement;
